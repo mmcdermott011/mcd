24afaae8694809f4ead28f8b563d4a160edd7249
michaelmcdermott <mmcdermott011@gmail.com>
2021-02-13 21:59:47 -0800

    freakin wsgi

--- mcd_web_portfolio/mcd_web_portfolio/wsgi.py
+++ mcd_web_portfolio/mcd_web_portfolio/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mcd_web_portfolio.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mcd_web_portfolio.mcd_web_portfolio.settings')
 
 application = get_wsgi_application()
