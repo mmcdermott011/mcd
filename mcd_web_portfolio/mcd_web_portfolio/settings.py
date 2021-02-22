66aecbb2c478aa3d187b6cda6017181759b34f4e
michaelmcdermott <mmcdermott011@gmail.com>
2021-02-21 21:58:56 -0800

    playing with dockerfile

--- mcd_web_portfolio/mcd_web_portfolio/settings.py
+++ mcd_web_portfolio/mcd_web_portfolio/settings.py
@@ -134,7 +134,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATIC_URL = 'mysite/static/'
+STATIC_URL = '/mysite/static/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'mysite/static'),
 )
