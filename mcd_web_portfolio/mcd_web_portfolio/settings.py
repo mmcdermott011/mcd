1636a4b104f0bb1591821a0bdd4a6cb448a87baf
michaelmcdermott <mmcdermott011@gmail.com>
2021-02-21 21:02:30 -0800

    added staticfiles dir

--- mcd_web_portfolio/mcd_web_portfolio/settings.py
+++ mcd_web_portfolio/mcd_web_portfolio/settings.py
@@ -134,8 +134,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATIC_URL = 'mysite/static/'
+STATIC_URL = 'mysite/static/mysite/'
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'mysite/static'),
+    os.path.join(BASE_DIR, 'mysite/static/mysite'),
 )
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
