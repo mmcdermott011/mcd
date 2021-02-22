48fa1bc3848792bfc76b0f35e8d1763c27c244ac
michaelmcdermott <mmcdermott011@gmail.com>
2021-02-21 20:13:14 -0800

    added whitenoise

--- mcd_web_portfolio/mcd_web_portfolio/settings.py
+++ mcd_web_portfolio/mcd_web_portfolio/settings.py
@@ -50,6 +50,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'mcd_web_portfolio.urls'
@@ -137,3 +138,4 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'mysite/static'),
 )
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
