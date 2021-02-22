5f6beecab47f2f44e9b74903efc428628fd32669
michaelmcdermott <mmcdermott011@gmail.com>
2021-02-21 21:12:21 -0800

    still figurin out this static sheesh

--- mcd_web_portfolio/mcd_web_portfolio/settings.py
+++ mcd_web_portfolio/mcd_web_portfolio/settings.py
@@ -134,8 +134,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATIC_URL = 'mysite/static/mysite/'
+STATIC_URL = 'mysite/static/'
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'mysite/static/mysite'),
+    os.path.join(BASE_DIR, 'mysite/static'),
 )
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
