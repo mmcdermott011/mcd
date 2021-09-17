3e73066489674683f7597c91daf51377ab9b7528
michaelmcdermott <mmcdermott011@gmail.com>
2021-09-16 21:01:38 -0700

    _"

--- mcd_web_portfolio/mcd_web_portfolio/settings.py
+++ mcd_web_portfolio/mcd_web_portfolio/settings.py
@@ -9,6 +9,7 @@ https://docs.djangoproject.com/en/3.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.0/ref/settings/
 """
+import django_heroku
 
 import os
 from dotenv import load_dotenv
@@ -139,3 +140,6 @@ STATIC_URL = '/mysite/static/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'mysite/static'),
 )
+
+
+django_heroku.settings(locals())
